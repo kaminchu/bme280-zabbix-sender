98922e3b5a5d4d183a554db625a5e67be1df3060
kaminchu <kmchu.mail@fmail.com>
2017-08-08 12:41:28 +0000

    add flow

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
+// @flow
 import cron from "node-cron";
 import BME280 from "node-bme280";
 import ZabbixSender from "node-zabbix-sender";
 import {} from "dotenv/config";
+
+import type {ProcessEnv} from "./types";
 
 const {
   HOST,
   PORT,
   ZABBIX_HOST,
   ZABBIX_ITEM_KEY
-} = process.env;
+}: ProcessEnv = process.env;
 
 const bme280 = new BME280({ address: 0x76 });
-const Sender = new ZabbixSender({ host: HOST, port: PORT });
+const Sender = new ZabbixSender({ host: HOST, port: parseInt(PORT, 10 ) });
+
+bme280.begin((err) => {
+  if (err) {
+    console.error("bme280 initializing error", err);      
+    throw err;
+  }
+});
 
 cron.schedule("* * * * *", () => {
 
-  bme280.begin((err) => {
-    if (err) {
-      console.error("bme280 initializing error", err);      
-      throw err;
-    }
     bme280.readPressureAndTemparature((_err, pressure, temperature, humidity) => {
       if (_err) {
         console.error("bme280 read error", _err);      
-        throw err;
+        throw _err;
       }
       Sender.addItem(ZABBIX_HOST, ZABBIX_ITEM_KEY, parseFloat(temperature));
       Sender.send((__err, res) => {
@@ -36,5 +41,5 @@ cron.schedule("* * * * *", () => {
       });
     });
   });
-});
+
 
