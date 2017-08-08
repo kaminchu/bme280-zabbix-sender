2f7fe9ab0fcd5258f6e220ef2d8783b1c0c11d0b
kaminchu <kmchu.mail@gmail.com>
2017-08-08 22:09:54 +0900

    add hum press

--- src/index.js
+++ src/index.js
@@ -4,42 +4,45 @@ import BME280 from "node-bme280";
 import ZabbixSender from "node-zabbix-sender";
 import {} from "dotenv/config";
 
-import type {ProcessEnv} from "./types";
 
 const {
-  HOST,
-  PORT,
+  SERVER_HOST,
+  SERVER_PORT,
   ZABBIX_HOST,
-  ZABBIX_ITEM_KEY
-}: ProcessEnv = process.env;
+  ZABBIX_ITEM_KEY_TEMP,
+  ZABBIX_ITEM_KEY_HUM,
+  ZABBIX_ITEM_KEY_PRESS,
+  CRON_STRING
+} = process.env;
 
 const bme280 = new BME280({ address: 0x76 });
-const Sender = new ZabbixSender({ host: HOST, port: parseInt(PORT, 10 ) });
+const Sender = new ZabbixSender({ host: SERVER_HOST, port: parseInt(SERVER_PORT, 10) });
 
 bme280.begin((err) => {
   if (err) {
-    console.error("bme280 initializing error", err);      
+    console.error("bme280 initializing error", err);
     throw err;
   }
 });
 
-cron.schedule("* * * * *", () => {
+cron.schedule(CRON_STRING, () => {
 
-    bme280.readPressureAndTemparature((_err, pressure, temperature, humidity) => {
-      if (_err) {
-        console.error("bme280 read error", _err);      
-        throw _err;
+  bme280.readPressureAndTemparature((_err, pressure, temperature, humidity) => {
+    if (_err) {
+      console.error("bme280 read error", _err);
+      throw _err;
+    }
+    Sender.addItem(ZABBIX_HOST, ZABBIX_ITEM_KEY_TEMP, temperature);
+    Sender.addItem(ZABBIX_HOST, ZABBIX_ITEM_KEY_HUM, humidity);
+    Sender.addItem(ZABBIX_HOST, ZABBIX_ITEM_KEY_PRESS, pressure / 100);
+    Sender.send((__err, res) => {
+      if (__err) {
+        console.error("zabbix send error");
+        throw __err;
       }
-      Sender.addItem(ZABBIX_HOST, ZABBIX_ITEM_KEY, parseFloat(temperature));
-      Sender.send((__err, res) => {
-        if (__err) {
-          console.error("zabbix send error");
-          throw __err;
-        }
-        console.log("data sended. temperature: " + temperature );
-        console.log(res);
-      });
+      console.log(res);
     });
   });
+});
 
 
