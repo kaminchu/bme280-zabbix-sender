2aa070968671bd5c5fa86d7ec8e930241f6aa5e6
kaminchu <kmchu.mail@gmail.com>
2018-12-17 22:16:16 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const options = {
   i2cBusNo   : 1, // defaults to 1
   i2cAddress : 0x76
 };
-const bme280 = new BME280();
+const bme280 = new BME280(options);
 const Sender = new ZabbixSender({ host: SERVER_HOST, port: parseInt(SERVER_PORT || "10051", 10) });
 
 const readSensorData = () => {
